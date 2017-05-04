0d6bf25d331b3ac977aefaacf12b0a2094af5039
Silvano Garnerone <silvano.garnerone@gmail.com>
2017-05-04 15:05:47 +0200

    added genomic and proteomic strings

--- genomicRanges.R
+++ genomicRanges.R
@@ -7,15 +7,22 @@ library(BSgenome.Hsapiens.UCSC.hg19)
 library(TxDb.Hsapiens.UCSC.hg19.knownGene)
 txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
 txdb
-transcripts(txdb)
+cdslist <- cdsBy(txdb, by = "gene")
+length(cdslist)
+names(cdslist)[10:13]
+cdslist[11:12]
 
-# ###################################################
-# ### GeneModel from Ensembl
-# ###################################################
-# library(GenomicFeatures)
-# txdb <- makeTxDbFromBiomart(biomart="ensembl",dataset="hsapiens_gene_ensembl")
-# txdb
+txlist <- transcriptsBy(txdb, by = "gene")
+length(txlist)
+names(txlist)[10:13]
+txlist[11:12]
 
-exonsBy(txdb, by="gene")
-genelist <- genes(txdb, columns="gene_id", filter=NULL, single.strand.genes.only=FALSE)
-genelist
+cds_seqs <- extractTranscriptSeqs(Hsapiens,cdsBy(txdb, by="tx",use.names=TRUE))
+cds_seqs
+translate(cds_seqs)
+##########################
+# supportedUCSCtables(genome="hg19")
+# hg19KG_txdb <- makeTxDbFromUCSC(genome = "hg19", tablename = "knownGene")
+# head(getChromInfoFromUCSC("hg19"))
+# saveDb(hg19KG_txdb,file = "hg19tx.sqlite")
+# hg19KG_txdb <- loadDb("hg19tx.sqlite")
