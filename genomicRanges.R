67dda6dcf2766708f2ff5d7624c325397b96c732
Silvano Garnerone <silvano.garnerone@gmail.com>
2017-05-04 16:23:00 +0200

    get sequences
    how to get exons and introns sequences

--- genomicRanges.R
+++ genomicRanges.R
@@ -26,3 +26,30 @@ translate(cds_seqs)
 # head(getChromInfoFromUCSC("hg19"))
 # saveDb(hg19KG_txdb,file = "hg19tx.sqlite")
 # hg19KG_txdb <- loadDb("hg19tx.sqlite")
+
+###################
+# Extract exons and introns for a gene
+###################
+library(TxDb.Hsapiens.UCSC.hg19.knownGene)
+txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+txdb
+
+library(GenomicFeatures)
+track2 <- "66008" #select a gene with entrez id
+# get transcript for a given gene
+track2_txs <- transcriptsBy(txdb, by="gene")[[track2]]
+track2_txs
+# get transcripts names
+track2_tx_names <- mcols(track2_txs)$tx_name
+track2_tx_names
+# extract the exon region
+track2_exbytx <- exonsBy(txdb,"tx",use.names=TRUE)[track2_tx_names]
+elementNROWS(track2_exbytx)
+# extract introns
+track2_inbytx <- intronsByTranscript(txdb,use.names=TRUE)[track2_tx_names]
+elementNROWS(track2_inbytx)
+# get DNA sequences
+library(BSgenome.Hsapiens.UCSC.hg19)
+trak2_ex_seqs <- getSeq(Hsapiens,track2_exbytx) # from exons
+trak2_in_seqs <- getSeq(Hsapiens,track2_inbytx) # from introns
+trak2_ex_seqs[["uc002uyb.4"]]
