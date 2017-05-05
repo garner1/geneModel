35c2522830b149273aff49826095062431e43126
garner1 <silvano.garnerone@gmail.com>
2017-05-05 17:48:04 +0200

    added functionality: obtain range and sequence of a gene with entrez ID

--- genomicRanges.R
+++ genomicRanges.R
@@ -1,55 +1,30 @@
 library(GenomicRanges)
-library(BSgenome.Hsapiens.UCSC.hg19)
-
-###################################################
-### GeneModel from UCSC
-###################################################
-library(TxDb.Hsapiens.UCSC.hg19.knownGene)
-txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
-txdb
-cdslist <- cdsBy(txdb, by = "gene")
-length(cdslist)
-names(cdslist)[10:13]
-cdslist[11:12]
-
-txlist <- transcriptsBy(txdb, by = "gene")
-length(txlist)
-names(txlist)[10:13]
-txlist[11:12]
-
-cds_seqs <- extractTranscriptSeqs(Hsapiens,cdsBy(txdb, by="tx",use.names=TRUE))
-cds_seqs
-translate(cds_seqs)
-##########################
-# supportedUCSCtables(genome="hg19")
-# hg19KG_txdb <- makeTxDbFromUCSC(genome = "hg19", tablename = "knownGene")
-# head(getChromInfoFromUCSC("hg19"))
-# saveDb(hg19KG_txdb,file = "hg19tx.sqlite")
-# hg19KG_txdb <- loadDb("hg19tx.sqlite")
-
 ###################
 # Extract exons and introns for a gene
 ###################
+# extract introns
+# get DNA sequences
+# ## Write an XStringSet object to a FASTA (or FASTQ) file:
+# library(Biostrings)
+# for (ind in seq(length(trak2_ex_seqs)) ){
+#   writeXStringSet(trak2_ex_seqs[[ind]], filepath='./test.fa', append=TRUE,compress=FALSE, compression_level=NA, format="fasta")  
+# }
+
 library(TxDb.Hsapiens.UCSC.hg19.knownGene)
 txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
-txdb
 
 library(GenomicFeatures)
-track2 <- "66008" #select a gene with entrez id
-# get transcript for a given gene
-track2_txs <- transcriptsBy(txdb, by="gene")[[track2]]
-track2_txs
-# get transcripts names
-track2_tx_names <- mcols(track2_txs)$tx_name
-track2_tx_names
-# extract the exon region
-track2_exbytx <- exonsBy(txdb,"tx",use.names=TRUE)[track2_tx_names]
-elementNROWS(track2_exbytx)
-# extract introns
-track2_inbytx <- intronsByTranscript(txdb,use.names=TRUE)[track2_tx_names]
-elementNROWS(track2_inbytx)
-# get DNA sequences
+# get transcript list grouped by gene
+transByGene <- transcriptsBy(txdb, by="gene",use.names=FALSE) 
+# transByGene[['66008']]
+transByGene
+# get DNA sequence for each transcript for each gene
 library(BSgenome.Hsapiens.UCSC.hg19)
-trak2_ex_seqs <- getSeq(Hsapiens,track2_exbytx) # from exons
-trak2_in_seqs <- getSeq(Hsapiens,track2_inbytx) # from introns
-trak2_ex_seqs[["uc002uyb.4"]]
+transByGene_seqs <- getSeq(Hsapiens,transByGene)
+# get the entrez gene ID of all the genes in the list
+entrezGID <- names(transByGene)
+# Given an index of the list of genes, from 1 to length(entrezGID), one can obtain the entrez ID, the range and the seqs of the gene transcripts
+ind <- 1 
+entrezGID[ind]
+transByGene[entrezGID[ind]] 
+transByGene_seqs[[ind]]
