b3184211287f90722ce943bf83b02800b867fd04
garner1 <silvano.garnerone@gmail.com>
2017-05-15 14:29:31 +0200

    create fasta file from full list of exon sequences associated to all gene entrez ID

--- genomicRanges.R
+++ genomicRanges.R
@@ -1,30 +1,31 @@
+###################
+# Extract exons for a gene
+###################
 library(GenomicRanges)
-###################
-# Extract exons and introns for a gene
-###################
-# extract introns
-# get DNA sequences
-# ## Write an XStringSet object to a FASTA (or FASTQ) file:
-# library(Biostrings)
-# for (ind in seq(length(trak2_ex_seqs)) ){
-#   writeXStringSet(trak2_ex_seqs[[ind]], filepath='./test.fa', append=TRUE,compress=FALSE, compression_level=NA, format="fasta")  
-# }
-
 library(TxDb.Hsapiens.UCSC.hg19.knownGene)
-txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
-
 library(GenomicFeatures)
-# get transcript list grouped by gene
-transByGene <- transcriptsBy(txdb, by="gene",use.names=FALSE) 
-# transByGene[['66008']]
-transByGene
-# get DNA sequence for each transcript for each gene
 library(BSgenome.Hsapiens.UCSC.hg19)
-transByGene_seqs <- getSeq(Hsapiens,transByGene)
-# get the entrez gene ID of all the genes in the list
-entrezGID <- names(transByGene)
-# Given an index of the list of genes, from 1 to length(entrezGID), one can obtain the entrez ID, the range and the seqs of the gene transcripts
-ind <- 1 
-entrezGID[ind]
-transByGene[entrezGID[ind]] 
-transByGene_seqs[[ind]]
+library(org.Hs.eg.db)
+
+txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
+exonsByGene <- exonsBy(txdb, by="gene", use.names=FALSE) # get exon list grouped by gene
+exonsByGene_seqs <- getSeq(Hsapiens,exonsByGene) # get DNA sequence for each exon for each gene
+entrezID <- names(exonsByGene) # get the entrez gene ID of all the genes in the list
+
+## Bimap interface:
+x <- org.Hs.egENSEMBL
+mapped_genes <- mappedkeys(x) # Get the entrez gene IDs that are mapped to an Ensembl ID
+xx <- as.list(x[mapped_genes]) # Convert to a list
+
+# Given an index of the list of genes, from 1 to length(entrezID), one can obtain the entrez ID, 
+# the ensemble ID, the range and the seqs of the gene transcripts
+for(ind in seq(1,length(entrezID))){
+print(ind)
+entrezID[ind] # the entrez ID
+# xx[entrezID[ind]] # get the ensemble ID
+# exonsByGene[entrezID[ind]] # the range on the genome
+# exonsByGene_seqs[[ind]] # the sequence
+# Write to fasta format
+out.fasta <- paste("~/Work/dataset/gene2vec/genedoc/", entrezID[ind],".fasta", sep="")
+writeXStringSet(exonsByGene_seqs[[ind]], out.fasta, format="fasta")
+}
